320da5302161ddb9949afb67907f56aee8b8f334
BigBlueDot <BigBlueDotGit@gmail.com>
2015-06-11 17:13:48 -0500

    Added KOEndingCondition Victory Condition
    fixes #12

--- LegitQuest/BattleService/Actors/EndingConditions/KOEndingCondition.cs
+++ LegitQuest/BattleService/Actors/EndingConditions/KOEndingCondition.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BattleService.InternalMessage;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,47 @@ using System.Threading.Tasks;
 
 namespace BattleService.Actors.EndingConditions
 {
-    public class KOEndingCondition
+    public class KOEndingCondition : Actor
     {
+        private List<Guid> allies;
+        private List<Guid> enemies;
 
+        public KOEndingCondition(List<Guid> allies, List<Guid> enemies)
+        {
+            this.allies = allies;
+            this.enemies = enemies;
+        }
+
+        public override void process(int time)
+        {
+            while (messages.Count >= 0)
+            {
+                MessageDataStructures.Message message = messages.Dequeue();
+
+                if (message is Defeated)
+                {
+                    Defeated specificMessage = (Defeated)message;
+                    if (allies.Contains(specificMessage.id))
+                    {
+                        allies.Remove(specificMessage.id);
+                    }
+                    else if (enemies.Contains(specificMessage.id))
+                    {
+                        enemies.Remove(specificMessage.id);
+                    }
+
+                    if (allies.Count == 0)
+                    {
+                        BattleDefeat battleDefeat = new BattleDefeat();
+                        addOutgoingMessage(battleDefeat);
+                    }
+                    else if (enemies.Count == 0)
+                    {
+                        BattleVictory battleVictory = new BattleVictory();
+                        addOutgoingMessage(battleVictory);
+                    }
+                }
+            }
+        }
     }
 }
