651a33c6a5c60def83a27f2a54b3b21a18335f3c
BigBlueDot <BigBlueDotGit@gmail.com>
2015-06-10 22:03:19 -0500

    Added Player character
    fixes #9

--- LegitQuest/BattleService/Actors/Characters/PlayerCharacter.cs
+++ LegitQuest/BattleService/Actors/Characters/PlayerCharacter.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BattleService.InternalMessage.Abilities;
+using MessageDataStructures;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,34 @@ namespace BattleService.Actors.Characters
 {
     public class PlayerCharacter : Character
     {
+        private bool canUseCommand()
+        {
+            return (this.castTimeComplete <= this.currentTime);
+        }
 
+        private bool AoEAttackAllowed(CommandIssued command)
+        {
+            return false; //In the future, will check the ability api or something
+        }
+
+        protected override void processMessage(Message message)
+        {
+            if (message is CommandIssued)
+            {
+                if (canUseCommand())
+                {
+                    CommandIssued specificMessage = (CommandIssued)message;
+                    
+                    //For now we are just assuming it's an attack
+                    PhysicalAttack physicalAttack = new PhysicalAttack();
+                    physicalAttack.abilityStrength = 15;
+                    physicalAttack.attack = this.strength;
+                    physicalAttack.id = specificMessage.target;
+                    addOutgoingMessage(physicalAttack);
+                }
+            }
+
+            base.processMessage(message);
+        }
     }
 }
