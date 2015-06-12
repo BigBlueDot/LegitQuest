6ba84db6914d8b81d04b6b755b371b68643df19c
BigBlueDot <BigBlueDotGit@gmail.com>
2015-06-11 23:04:02 -0500

    Added DefenseIncreased Status Actor
    fixes #14

---
+++ LegitQuest/BattleService/Actors/Statuses/DefenseIncreased.cs
@@ -0,0 +1,40 @@
+﻿using BattleService.InternalMessage;
+using BattleService.InternalMessage.Abilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleService.Actors.Statuses
+{
+    public class DefenseIncreased : Actor
+    {
+        private int endTime { get; set; }
+        private int defenseMod { get; set; }
+        private Guid target { get; set; }
+
+        public DefenseIncreased(int currentTime, int duration, int defenseMod, Guid target)
+        {
+            this.endTime = currentTime + duration;
+            this.defenseMod = defenseMod;
+            this.target = target;
+        }
+
+        public override void process(int time)
+        {
+            if (time >= endTime)
+            {
+                DefenseDecreased defenseDecreased = new DefenseDecreased();
+                defenseDecreased.defenseReduction = defenseMod;
+                defenseDecreased.duration = -1;
+                defenseDecreased.id = this.target;
+                this.addOutgoingMessage(defenseDecreased);
+
+                Defeated defeated = new Defeated();
+                defeated.id = this.id;
+                this.addOutgoingMessage(defeated);
+            }
+        }
+    }
+}
