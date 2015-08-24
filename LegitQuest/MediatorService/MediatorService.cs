a5f9890ec9ec699bfe9498acbb96b14745fb1c5e
BigBlueDot <BigBlueDotGit@gmail.com>
2015-08-23 19:47:53 -0500

    Added a mediator class
    fixes #19

---
+++ LegitQuest/MediatorService/MediatorService.cs
@@ -0,0 +1,32 @@
+﻿using MessageDataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediatorService
+{
+    public class MediatorService
+    {
+        private Dictionary<ServiceType, MessageDataStructures.MessageWriter> writers;
+        private MessageReader messageReader;
+
+        public MediatorService()
+        {
+            initWriters();
+        }
+
+        private void initWriters()
+        {
+            writers = new Dictionary<ServiceType, MessageDataStructures.MessageWriter>();
+            //TODO:  Add writers here
+        }
+
+        private void initReader()
+        {
+            //TODO:  Add reader (or readers, maybe?  I haven't decided if there will be one queue for the mediator or one per service)f
+
+        }
+    }
+}
