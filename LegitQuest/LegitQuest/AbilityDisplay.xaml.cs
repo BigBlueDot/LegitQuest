e6ebabaacc88c6ee6690c1f5463b1afdaefef6b2
BigBlueDot <BigBlueDotGit@gmail.com>
2015-08-24 18:33:01 -0500

    Added AbilityDisplay control
    fixes #63

--- LegitQuest/LegitQuest/AbilityDisplay.xaml.cs
+++ LegitQuest/LegitQuest/AbilityDisplay.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LegitQuest.EventInfo;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,30 @@ namespace LegitQuest
     /// </summary>
     public partial class AbilityDisplay : UserControl
     {
-        public AbilityDisplay()
+        private Guid characterId;
+        private int commandNumber;
+        public event AbilitySelectedEventHandler AbilitySelected;
+        public delegate void AbilitySelectedEventHandler(AbilitySelectedEventArgs e);
+
+
+        public AbilityDisplay(Guid characterId, String name, int commandNumber)
         {
             InitializeComponent();
+            Ability.Content = name;
+            this.characterId = characterId;
+            this.commandNumber = commandNumber;
+        }
+
+        private void Ability_Click_1(object sender, RoutedEventArgs e)
+        {
+            AbilitySelectedEventArgs args = new AbilitySelectedEventArgs();
+            args.commandIssued = new MessageDataStructures.CommandIssued();
+            args.commandIssued.source = characterId;
+            args.commandIssued.commandNumber = this.commandNumber;
+            if (AbilitySelected != null)
+            {
+                AbilitySelected(args);
+            }
         }
     }
 }
---
+++ LegitQuest/LegitQuest/EventArgs/AbilitySelectedEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using MessageDataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegitQuest.EventInfo
+{
+    public class AbilitySelectedEventArgs : EventArgs
+    {
+        public CommandIssued commandIssued { get; set; }
+    }
+}
