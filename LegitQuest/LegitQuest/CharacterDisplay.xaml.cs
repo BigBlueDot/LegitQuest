70bbe6bd29d93f12968225038dd5fe9764a38f21
BigBlueDot <BigBlueDotGit@gmail.com>
2015-08-24 18:55:12 -0500

    Added CharacterDisplay control
    fixes #64

--- LegitQuest/LegitQuest/CharacterDisplay.xaml.cs
+++ LegitQuest/LegitQuest/CharacterDisplay.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LegitQuest.EventInfo;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,39 @@ namespace LegitQuest
     /// </summary>
     public partial class CharacterDisplay : UserControl
     {
+        private int hp;
+        private int maxHP;
+        private string name;
+        private Guid characterId;
+
+        public delegate void CharacterClickedEventHandler(CharacterSelectedEventArgs args);
+        public event CharacterClickedEventHandler CharacterClicked;
+
         public CharacterDisplay(MessageDataStructures.ViewModels.Character character)
         {
             InitializeComponent();
+            UpdateCharacter(character);
+        }
+
+        public void UpdateCharacter(MessageDataStructures.ViewModels.Character character)
+        {
+            this.hp = character.hp;
+            this.maxHP = character.maxHp;
+            this.name = "Name";
+            this.characterId = character.id;
+
+            this.txtName.Text = this.name;
+            this.txtHP.Text = this.hp.ToString() + " / " + this.maxHP.ToString();
+        }
+
+        private void Select_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (CharacterClicked != null)
+            {
+                CharacterSelectedEventArgs args = new CharacterSelectedEventArgs();
+                args.id = this.characterId;
+                CharacterClicked(args);
+            }
         }
     }
 }
---
+++ LegitQuest/LegitQuest/EventInfo/CharacterSelectedEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegitQuest.EventInfo
+{
+    public class CharacterSelectedEventArgs : EventArgs
+    {
+        public Guid id { get; set; }
+    }
+}
