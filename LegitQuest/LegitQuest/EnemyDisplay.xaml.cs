c817a910e39577637bc00f9444a469f3518741be
BigBlueDot <BigBlueDotGit@gmail.com>
2015-08-24 18:56:56 -0500

    Added EnemyDisplay control
    fixes #65

--- LegitQuest/LegitQuest/EnemyDisplay.xaml.cs
+++ LegitQuest/LegitQuest/EnemyDisplay.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LegitQuest.EventInfo;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,39 @@ namespace LegitQuest
     /// </summary>
     public partial class EnemyDisplay : UserControl
     {
+        private int hp;
+        private int maxHP;
+        private string name;
+        private Guid characterId;
+
+        public delegate void CharacterClickedEventHandler(CharacterSelectedEventArgs args);
+        public event CharacterClickedEventHandler EnemyClicked;
+
         public EnemyDisplay(MessageDataStructures.ViewModels.Character character)
         {
             InitializeComponent();
+            UpdateEnemy(character);
+        }
+
+        public void UpdateEnemy(MessageDataStructures.ViewModels.Character enemy)
+        {
+            this.hp = enemy.hp;
+            this.maxHP = enemy.maxHp;
+            this.name = "Name";
+            this.characterId = enemy.id;
+
+            this.txtName.Text = this.name;
+            this.txtHP.Text = this.hp.ToString() + " / " + this.maxHP.ToString();
+        }
+
+        private void Select_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (EnemyClicked != null)
+            {
+                CharacterSelectedEventArgs args = new CharacterSelectedEventArgs();
+                args.id = this.characterId;
+                EnemyClicked(args);
+            }
         }
     }
 }
