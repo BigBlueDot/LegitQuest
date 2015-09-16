984008cb09c6fb86b1115d6db6a13fda0c99c523
BigBlueDot <BigBlueDotGit@gmail.com>
2015-09-16 18:15:30 -0500

    Added blank MVC project
    fixes #110

---
+++ LegitQuest/LegitQuestWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LegitQuestWebApp.Startup))]
+namespace LegitQuestWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
