9b883b292121d9155d35e65c3714a7923fab68ff
BigBlueDot <BigBlueDotGit@gmail.com>
2015-09-19 16:08:44 -0500

    Added a messaging framework and got some basics working
    fixes #112

--- LegitQuest/LegitQuestWebApp/App_Start/BundleConfig.cs
+++ LegitQuest/LegitQuestWebApp/App_Start/BundleConfig.cs
@@ -11,6 +11,8 @@ namespace LegitQuestWebApp
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/signalR").Include("~/Scripts/jquery.signalR-2.2.0.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
---
+++ LegitQuest/LegitQuestWebApp/BattleHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace LegitQuestWebApp
+{
+    public class BattleHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+            Clients.All.broadcastMessage("something", "Something");
+        }
+    }
+}
