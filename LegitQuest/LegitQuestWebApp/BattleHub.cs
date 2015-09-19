7d55a0a7bd93e7c553af9538a5a8f252ae3f71a0
BigBlueDot <BigBlueDotGit@gmail.com>
2015-09-19 17:01:42 -0500

    Got battle messages temp working
    fixes #113

--- LegitQuest/LegitQuestWebApp/BattleHub.cs
+++ LegitQuest/LegitQuestWebApp/BattleHub.cs
@@ -3,15 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using MediatorServiceLibrary;
+using MessageDataStructures;
+using MessageDataStructures.BattleGeneration;
 
 namespace LegitQuestWebApp
 {
     public class BattleHub : Hub
     {
-        public void Hello()
+        private MediatorService mediatorService;
+        private DirectMessageReader messageWriter;
+        private DirectMessageReader messageReader;
+
+        public BattleHub()
         {
-            Clients.All.hello();
-            Clients.All.broadcastMessage("something", "Something");
+            this.messageReader = new DirectMessageReader();
+            this.messageWriter = new DirectMessageReader();
+            this.mediatorService = new MediatorService(ref messageReader, ref messageWriter);
+            this.messageReader.MessageReceived += MessageReader_MessageReceived;
+        }
+
+        public void startCombat()
+        {
+            BattleGenerationRequest battleGenerationRequest = new BattleGenerationRequest();
+            messageWriter.writeMessage(battleGenerationRequest);
+        }
+
+        public void useCommand(CommandIssued commandIssued)
+        {
+            messageWriter.writeMessage(commandIssued);
+        }
+
+        private void MessageReader_MessageReceived(MessageReceivedEventArgs args)
+        {
+            this.Clients.All.processMessage(args.message);
         }
     }
 }
